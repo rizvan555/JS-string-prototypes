168fbd8f2acef1ae394767e3ec16801559b78c2e
Rizvan555 <rizvan84@gmx.de>
2023-03-08 17:42:27 +0100

    all projects are finished

---
+++ lvl_1_1 / literals/assets/js/main.js
@@ -0,0 +1,13 @@
+const myFirstName = "Rizvan "
+const myLastName = "Karimov"
+let myAge = 39
+const birth = "Aserbaidschan"
+let size = 1.77
+let weight = 95
+let hobby = "Code schreiben"
+let food = "Kebap"
+let sport = "Armwrestling"
+let season = "Sommer"
+let holiday = "Heimat"
+
+document.write (`Mein Name ist ${myFirstName} ${myLastName} und ich bin ${myAge} Jahre alt. Meine Größe und mein Gewicht sind ${size}m und ${weight}kg. Mein Lieblingshobby ist ${hobby} und mein Lieblingsessen ist ${food} und als Lieblingssport gehe ich ${sport}. Meine Lieblingsjahreszeit ist ${season} und ich mache gerne Urlaub ${holiday}.`)
